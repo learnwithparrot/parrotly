3d17441a51c41f0f3dcf71b69a01cbc3da6dcf4b
Yonga Springfield <springfieldyonga@outlook.com>
2021-10-03 16:28:10 +0100

    included sample.env.ts

---
+++ sample.env.ts
@@ -0,0 +1,8 @@
+//REname file to env.ts and fill in your configs as sampled below
+export const FIREBASE_API_KEY = "xxxxxxxxxxxxxx"
+export const FIREBASE_AUTH_DOMAIN = "xxxxxxxxxxxxxx"
+export const FIREBASE_PROJECT_ID = "xxxxxxxxxxxxxx"
+export const FIREBASE_STORAGE_BUCKET = "xxxxxxxxxxxxxx"
+export const FIREBASE_MESSAGING_SENDER_ID = "xxxxxxxxxxxxxx"
+export const FIREBASE_APP_ID = "xxxxxxxxxxxxxx"
+export const FIREBASE_MEASUREMENT_ID = "xxxxxxxxxxxxxx"
