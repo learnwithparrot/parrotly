e49585f1f1c0b18cf6db056262b905c91b30e626
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-01-09 17:10:38 +0100

    created mcq component
    created mcq extension component and wired it up in component playground app.

--- apps/component-playground/src/App.svelte
+++ apps/component-playground/src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import {
     ExtensionSideNavPage,
-    WelcomePage,ExtensionSideNavLoginPage,
+    WelcomePage,
+    ExtensionSideNavLoginPage,
     ShowWordPage,
+    ShowMCQCardPage,
     AddToRepetitionList,
   } from './pages';
   import { Router, Route, Link } from 'svelte-navigator';
@@ -18,12 +20,14 @@
       <Link to="/show_word">Show Word</Link>
       <Link to="/add_to_repetition_list">Add to repetition list</Link>
       <Link to="/sidenav_login">Sidenav Login</Link>
+      <Link to="/show_mcq">Show MCQ</Link>
     </nav>
     <div>
       <Route path="/sidenav" component={ExtensionSideNavPage} />
       <Route path="/show_word" component={ShowWordPage} />
       <Route path="/add_to_repetition_list" component={AddToRepetitionList} />
       <Route path="/sidenav_login" component={ExtensionSideNavLoginPage} />
+      <Route path="/show_mcq" component={ShowMCQCardPage} />
       <Route component={WelcomePage} />
     </div>
   </Router>
---
+++ apps/component-playground/src/pages/ShowMCQCard.page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { MCQCard } from '@parrotly.io/extension-components';
+</script>
+
+<MCQCard
+  on:close={() => {}}
+  on:rightAnswer={() => {}}
+  on:wrongAnswer={() => {}}
+  on:knowWord={() => {}}
+  word="Long word standing here."
+  options={['Long word translation standing here.', 'Sencond option']}
+  translation="Third option"
+/>
