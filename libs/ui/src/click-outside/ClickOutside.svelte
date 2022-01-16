a7c4898c5e0a1b644470b5e32edfbad76f0cd0c3
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-01-16 13:07:39 +0100

    Created ClickOutside component

---
+++ libs/ui/src/click-outside/ClickOutside.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  export let container: HTMLDivElement;
+
+  const dispatch = createEventDispatcher();
+
+  const onWindowClick = (e: Event) => {
+    //@ts-ignore
+    if (!container.contains(e.target)) {
+      dispatch('clickOutside');
+    }
+  };
+</script>
+
+<svelte:window on:click={onWindowClick} />
+
+<template>
+  <slot />
+</template>
